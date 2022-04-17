ba73b6c8493129071b091c8de4835793e2f9257d
Stephen Petrick <stephenp1912@gmail.com>
2022-04-16 20:11:22 -0500

    Successfully implemented create and join project
    Moved join project functionality to a different function component, to
    prevent to async functions from being in the same function component.
    
    A Ref var can't be used in two different places for input. Apparently
    second input overwrites the first even if no input is received in the
    second.

--- src/project.js
+++ src/project.js
@@ -1,6 +1,7 @@
 import React, {useRef} from "react";
 import { useState } from "react";
 import {Link} from "react-router-dom";
+import currentProjectID from "./project_global";
 
 
 function Project_Board(){
@@ -13,18 +14,26 @@ function Project_Board(){
    const name_field = useRef();
    const description_field = useRef();
    const id_field = useRef();
+   const joinID_field = useRef();
 
+   /*
+    * Function to update the current state of name, id and description
+    * Was causing error if these statements were in one of the async functions
+    */
 
    async function createProject(){
       console.log("in display_props")
+
 
       let createProjectName = name_field.current.value;
       let createProjectID = id_field.current.value;
       let createProjectDescription =description_field.current.value;
 
-      console.log("name " + {createProjectName});
-      console.log("projectid: " + {createProjectID});
-      console.log("project description: "+{createProjectDescription});
+
+
+      console.log("name " + createProjectName);
+      console.log("projectid: " + createProjectID);
+      console.log("project description: "+createProjectDescription);
 
       let postDict = {
          method: "POST",
@@ -53,7 +62,7 @@ function Project_Board(){
       console.log(responseJson)
       
    }
-
+   
 
    return (
       <>
@@ -87,9 +96,16 @@ function Project_Board(){
          </form> 
 
          <button onClick = {() => createProject()}> Create new project </button>
+
+         <p className = "warning"> Do not use "!" or a space character </p>
+            
+            <GetProject/>
+         
+         <p> Server response for creating a project is: {serverResponse} </p>
          <p> New name: {project_name} </p>
          <p> New description: {project_description} </p>
          <p> New ID: {project_id} </p>
+
 
 
          <nav>
@@ -106,8 +122,101 @@ function Project_Board(){
 }
 
 export default Project_Board;
+function GetProject()
+{
+  const [serverResponse, setServerResponse] = useState("No Response Yet");
+  const namefield = useRef();
+  async function sendCredentials()
+  {
+    let id = namefield.current.value;
+    console.log("The id of the projec to get is : " + id);
+    let dict = {
+      method : "POST",
+      body : JSON.stringify({
+        id : id
+      })
+    };
+    let res = await fetch("/getProject" , dict);
+    let responseJson = await res.json();
+    if(responseJson['errorcode'] != 0)
+    {
+      setServerResponse("Fialure in retreival");
+      return;
+    }
+    let name = responseJson['name'];
+    let projectid = responseJson['projectid'];
+    let des = responseJson['description'];
+    setServerResponse("\nName: " + name + " \nProject id: " + projectid + " \nDescription: " + des);
+  }
+  return(
+    <>
+      <div>
+         <br></br>
+         <label>
+          Enter Project ID to join:
+          <input ref = {namefield} type = "text" placeholder = "Enter a project id" size = "21"></input>
+         </label>
+        </div>
+        <div>
+          <button onClick = {() => sendCredentials()}>Join a project</button>
+         <p>Server Response for joining a project is: {serverResponse}</p>
+      </div>
+    </>
+  );
+}
+/*
+ *
+         <button onClick = {() => createProject()}> Create new project </button>
+function joinProject(){
 
+   const [serverResponse, setServerResponse] = useState("No reponse yet");
+   const joinID_field = useRef();
+   async function getProject(){
+      
+      let join_ID = joinID_field.current.value;
 
+      let postDict = {
+         method: "POST",
+         body: JSON.stringify({
+            id: join_ID
+         })
+      };
+
+      let res = await fetch("/getProject", postDict);
+      let responseJson = await res.json();
+
+      console.log(responseJson)
+
+      if(responseJson['errorcode'] != 0)
+          {
+            setServerResponse("Fialure in retreival");
+            return;
+          }
+          let name = responseJson['name'];
+          let projectid = responseJson['projectid'];
+          let des = responseJson['description'];
+          setServerResponse("The name: " + name + " The project id: " + projectid + " THe description: " + des);
+
+   }
+
+      return (
+
+         <>
+            <form>
+
+               <label> Enter Project ID of project to join: 
+                  <input ref = {joinID_field} type = "text" placeholder = "Enter a project ID" />
+               </label>
+            
+            <button onClick = {() => getProject()}> Join a project </button>
+            <p> Server response: {serverResponse} </p>
+            </form>
+
+         </>
+      )
+
+}
+*/
 class Project extends React.Component{
    constructor(props){
       super (props);
---
+++ src/project_global.js
@@ -0,0 +1,4 @@
+const currentProjectID = 0;
+//exports variable, enabling it to be used as a global
+//variable via import statements
+export default currentProjectID
