6b8aa7ee9dd9b1f96f4fcb9433003c2122b95b1d
wildwilliam2 <willwooten325@gmail.com>
2022-04-16 23:14:48 -0500

    Dataset page displays the meta data upon request, and offers a link to download

--- src/Dataset.js
+++ src/Dataset.js
@@ -1,9 +1,16 @@
+import { SettingsInputHdmiTwoTone } from '@mui/icons-material';
 import React, { useState, useRef } from 'react';
 import {Link} from "react-router-dom";
 
 function Dataset()
 {
   const [serverResponse, setServerResponse] = useState("No Response Yet");
+  const [databaseName, setDatabaseName] = useState("");
+  const [description, setDescription] = useState("");
+  const [entries, setEntries] = useState("");
+  const [unit, setUnit] = useState("");
+  const [link, setLink] = useState("");
+
   const datasetNumField = useRef();
 
   async function requestMetaData()
@@ -26,21 +33,43 @@ function Dataset()
       setServerResponse("Meta data failed to load; try different input");
       return;
     }
+    console.log(responseJson);
     let newDisplay = "";
     newDisplay = "Database: " + responseJson['Database'] + " Description: " + responseJson['Description'] + " Entries: " + responseJson["Entries"] + " Unit: " + responseJson["Unit"] + " Link: " + responseJson["Link"];
-    setServerResponse(newDisplay);
+    //setServerResponse(newDisplay);
+    setDatabaseName(responseJson['Database']);
+    setEntries(responseJson['Entries']);
+    setDescription(responseJson['Description']);
+    setUnit(responseJson['Unit']);
+    setLink(responseJson['Link']);
+
   }
   return(
     <>
       <h2> Display a Dataset Metadata</h2>
       <div>
-          <h3>Enter the projectid, the hardware set name and quantity of hardware to request/return</h3>
+          <h3>Enter a number 1-5 to get different datasets</h3>
           <input ref = {datasetNumField} type = "text" placeholder = "Enter the number(1-5) of the dataset" size = "26"></input>
         </div>
         <div>
           <button onClick = {() => requestMetaData()}>Enter to return the amount</button>
       </div>
-      <label>{serverResponse}</label>
+      <div>
+        <label> The Database: {databaseName}</label>
+        <br/>
+        <br/>
+        <label> The Description: {description}</label>
+        <br/>
+        <br/>
+        <label> The Entries: {entries}</label>
+        <br/>
+        <br/>
+        <label> The Unit: {unit}</label>
+        <br/>
+        <br/>
+        <label> The Link: </label>
+        <p><a href={link}>{link}</a></p>
+      </div>
       <Link to = "/"> Home </Link>
       
     </>
